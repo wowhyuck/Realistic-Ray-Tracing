3a09a01ebf4d83e4621ca205685afed988d1bc33
Joohyuck <joohyuck.seo@gmail.com>
2020-01-21 12:56:13 +0900

    image class

---
+++ Image.cc
@@ -0,0 +1,136 @@
+// Image.cc
+
+#include "Image.h"
+#include <iostream>
+using namespace std;
+
+Image::Image()	{}
+
+Image::Image(int width, int height)
+{
+	nx = width;
+	ny = height;
+
+	// allocate memory for raster
+	raster = new rgb * [nx];
+	for (int i = 0; i < nx; i++)
+		raster[i] = new rgb[ny];
+}
+
+Image::Image(int width, int height, rgb background)
+{
+	nx = width;
+	ny = height;
+
+	// allocate memory for raster
+	raster = new rgb * [nx];
+	for (int i = 0; i < nx; i++)
+	{
+		raster[i] = new rgb[ny];
+
+		// assign 'background' to each element
+		for (int j = 0; j < ny; j++)
+			raster[i][j] = background;
+	}
+}
+
+bool Image::set(int x, int y, const rgb& color)
+{
+	// check for out of bounds error
+	if (0 > x || x > nx) return false;
+	if (0 > y || y > ny) return false;
+
+	raster[x][y] = color;
+}
+
+void Image::gammaCorrect(float gamma)
+{
+	rgb temp;
+	float power = 1.0 / gamma;
+	for (int i=0; i<nx; i++)
+		for (int j = 0; j < ny; j++)
+		{
+			temp = raster[i][j];
+			raster[i][j] = rgb(pow(temp.r(), power),
+							pow(temp.g(), power),
+							pow(temp.b(), power));
+		}
+
+}
+
+void Image::writePPM(ostream& out)
+{
+	// output header
+	out << "P6\n";
+	out << nx << " " << ny << "\n";
+	out << "255\n";
+	int i, j;
+	unsigned int ired, igreen, iblue;
+	unsigned char red, green, blue;
+
+	// output clamped [0, 255] values
+	for(i=ny-1; i>=0; i--)
+		for (j = 0; j < nx; j++)
+		{
+			ired = (unsigned int)(256 * raster[j][i].r());
+			igreen = (unsigned int)(256 * raster[j][i].g());
+			iblue = (unsigned int)(256 * raster[j][i].b());
+			if (ired > 255) ired = 255;
+			if (igreen > 255) igreen = 255;
+			if (iblue > 255) iblue = 255;
+			red = (unsigned char)(ired);
+			green = (unsigned char)(igreen);
+			blue = (unsigned char)(iblue);
+			out.put(red);
+			out.put(green);
+			out.put(blue);
+		}
+}
+
+// rad in a binary PPM
+void Image::readPPM(string file_name)
+{
+	// open stream to file
+	ifstream in;
+	in.open(file_name.c_str());
+	if (!in.is_open())
+	{
+		cerr << " ERROR -- Couldn't open file \'" << file_name << "\'.\n";
+		exit(-1);
+	}
+
+	char ch, type;
+	char red, green, blue;
+	int i, j, cols, rows;
+	int num;
+
+	// read in header info
+	in.get(ch);
+	in.get(type);
+	in >> cols >> rows >> num;
+
+	nx = cols;
+	ny = rows;
+
+	// allocate raster
+	raster = new rgb * [nx];
+	for (i = 0; i < nx; i++)
+		raster[i] = new rgb[ny];
+
+	// clean up newline
+	in.get(ch);
+
+	// store PPM pixel valuesin raster
+	for(i=ny-1; i>=0; i--)
+		for (j = 0; j < nx; j++)
+		{
+			in.get(red);
+			in.get(green);
+			in.get(blue);
+			raster[j][i] =
+				rgb((float)((unsigned char)red) / 255.0,
+					(float)((unsigned char)green) / 255.0,
+					(float)((unsigned char)blue) / 255.0);
+		}
+}
+
---
+++ Image.h
@@ -0,0 +1,33 @@
+#pragma once
+// Image.h
+
+#include <cmath>
+#include <string>
+#include <fstream>
+#include "rgb.h"
+using namespace std;
+
+class Image
+{
+public:
+	Image();
+
+	// initializes raster to default rgb color
+	Image(int width, int height);
+	
+	// initializes raster to 'background'
+	Image(int width, int height, rgb background);
+
+	// returns false if x or y are out of bounds, else true
+	bool set(int x, int y, const rgb& color);
+	void gammaCorrect(float gamma);
+	
+	// outputs PPM image to 'out'
+	void writePPM(ostream& out);
+	void readPPM(string file_name);
+
+private:
+	rgb** raster;
+	int nx;	// raster width
+	int ny;	// raster height
+};
