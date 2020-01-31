2cf027bee4e78cfab257b2ab0772b039cb585e44
Joohyuck <joohyuck.seo@gmail.com>
2020-01-31 16:12:08 +0900

    UVsphere class

---
+++ UVSphere.h
@@ -0,0 +1,20 @@
+#pragma once
+// UVSphere.h
+
+#include "Shape.h"
+#include "Vector3.h"
+#include "Ray.h"
+#include "Texture.h"
+
+class UVSphere : public Shape
+{
+public:
+	UVSphere(const Vector3& _center, float _radius, Texture* tex);
+	//BBox boundingBox() const;
+	bool hit(const Ray& r, float tmin, float tmax, float time, HitRecord& record) const;
+	bool shadowHit(const Ray& r, float tmin, float tmax, float time) const;
+
+	Vector3 center;
+	float radius;
+	Texture* tex;
+};
